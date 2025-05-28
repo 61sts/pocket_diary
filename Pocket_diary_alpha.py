ff189d5c90e2847d17d9563ffae7f2277def953f
61sts <over61sts@gmail.com>
2025-05-28 22:17:19 +0530

    swtiched to bridge commands, added info command and changed hello to list command

--- Pocket_diary_alpha.py
+++ Pocket_diary_alpha.py
@@ -1,27 +1,66 @@
 from keep_alive import keep_alive
 keep_alive()
 import discord
+from discord.ext import bridge
+from discord.ext import commands
 import os
 import sys
 from dotenv import load_dotenv
-intents = discord.Intents.default()
-intents.message_content = True  # Needed to read message content
-client = discord.Client(intents=intents)
+#* ====================================================================================================================================
 
-@client.event
+# Making bot object from Bridge class (Subclass of Client)
+bot = bridge.Bot(intents=discord.Intents.all(), command_prefix="-")
+
+# An event for when bot goes online
+@bot.event
 async def on_ready():
-  print("PocketDiary is ready to be used")
+  print(f"{bot.user} is ready to be used")
+  await bot.sync_commands()
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-  if message.content.startswith("-open"):
-    await message.channel.send("PocketDiary is open!")
-    
+# "help" command placeholder
+@bot.bridge_command(description="List of commands that bot has")
+async def list(ctx):
+    embed = discord.Embed(title="Command List",
+                          description="- `/info` : Provides information about the bot",
+                          color=discord.Colour.blurple()
+                          )
+    await ctx.respond(embed=embed)   
 
-# =================================================DO_NOT_MESS=================================================    
+# Info about the bot - "info" command - both / and -
+@bot.bridge_command(description="Gives information about the bot")
+async def info(ctx: discord.ApplicationContext):
+    embed = discord.Embed(title=f"Hi! I am Pocket Diary",
+                          description="A bot **currently in development** by <@1363200418552873024>.",
+                          color=discord.Colour.blurple()
+    )
+    embed.add_field(name="Coded in", value="Python", inline=True)
+    embed.add_field(name="Library", value="Pycord", inline=True)
+    embed.add_field(name="Created on", value="<t:1748347560:D>", inline=True)
+    embed.set_footer(text="Learning and Improving everyday")
+    embed.set_author(name="61sts")
+    embed.set_thumbnail(url="https://img.freepik.com/premium-vector/pixel-art-open-book-icon-game-asset-design_466450-2192.jpg")
+    embed.set_image(url="https://i.pinimg.com/originals/4f/d3/0e/4fd30efd8301e3551a3a63da0d9c4d88.gif")
+    await ctx.respond(embed=embed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#*             =================================================DO_NOT_MESS=================================================    
 if len(sys.argv) > 1 and sys.argv[1] == "test":
-    client.run(os.getenv("TEST_TOKEN"))
+    bot.run(os.getenv("TEST_TOKEN"))
 else:
-    client.run(os.getenv("TOKEN"))
+    bot.run(os.getenv("TOKEN"))
+
+# python Pocket_diary_alpha.py test
